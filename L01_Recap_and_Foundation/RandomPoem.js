fc0f26f39ae0833b0b70a52edff2228a3980f5a7
Caroco44 <caroline04kraemer@yahoo.de>
2024-03-29 12:57:29 +0100

    240329_2

--- L01_Recap_and_Foundation/RandomPoem.js
+++ L01_Recap_and_Foundation/RandomPoem.js
@@ -4,17 +4,18 @@ var RandomPoem;
     let subject = ["Der Rabe", "Die Katze", "Der Hund", "Der Fuchs"];
     let predicate = ["mag", "hasst", "liebt", "vergisst"];
     let object = ["die Natur", "den Wald", "den Baum", "die Höhle"];
-    for (let i = 3; i >= 0; i--) {
-        getVerse(subject, predicate, object);
+    for (let i = subject.length; i >= 1; i--) {
+        let poem = getVerse(subject, predicate, object);
+        console.log(poem);
     }
     function getVerse(_subject, _predicate, _object) {
-        let randomNumber = Math.floor(Math.random() * 4);
-        // console.log(randomNumber);
-        let spliceSubject = _subject.splice(randomNumber, 1);
-        let splicePredicate = _predicate.splice(randomNumber, 1);
-        let spliceObject = _object.splice(randomNumber, 1);
-        let verse = spliceSubject[0] + " " + splicePredicate[0] + " " + spliceObject[0];
-        console.log(verse);
+        let verse = "";
+        let subjectVariable = Math.floor(Math.random() * _subject.length);
+        verse += _subject.splice(subjectVariable, 1)[0] + " ";
+        let predicateVariable = Math.floor(Math.random() * _predicate.length);
+        verse += _predicate.splice(predicateVariable, 1)[0] + " ";
+        let objectVariable = Math.floor(Math.random() * _object.length);
+        verse += _object.splice(objectVariable, 1)[0] + " ";
         return verse;
     }
 })(RandomPoem || (RandomPoem = {}));
--- L01_Recap_and_Foundation/RandomPoem.ts
+++ L01_Recap_and_Foundation/RandomPoem.ts
@@ -4,23 +4,24 @@ namespace RandomPoem {
   let predicate: string[] = ["mag", "hasst", "liebt", "vergisst"];
   let object: string[] = ["die Natur", "den Wald", "den Baum", "die Höhle"];
 
-
   for (let i = subject.length; i >= 1; i--) {
     let poem = getVerse(subject, predicate, object);
     console.log(poem);
-    }
-
+  }
 
   function getVerse(_subject: string[], _predicate: string[], _object: string[]) {
     let verse: string = "";
 
-    let subjectVariable: number = Math.floor(Math.random() * _subject.length);verse += _subject.splice(subjectVariable, 1) [0] + " ";
+    let subjectVariable: number = Math.floor(Math.random() * _subject.length);
+    verse += _subject.splice(subjectVariable, 1)[0] + " ";
 
-    let predicateVariable: number = Math.floor(Math.random() * _predicate.length);verse += _subject.splice(predicateVariable, 1) [0] + " ";
+    let predicateVariable: number = Math.floor(Math.random() * _predicate.length);
+    verse += _predicate.splice(predicateVariable, 1)[0] + " ";
 
-    let objectVariable: number = Math.floor(Math.random() * _object.length);verse += _subject.splice(objectVariable, 1) [0] + " ";
+    let objectVariable: number = Math.floor(Math.random() * _object.length);
+    verse += _object.splice(objectVariable, 1)[0] + " ";
 
-    return verse
+    return verse;
   }
 
 }
