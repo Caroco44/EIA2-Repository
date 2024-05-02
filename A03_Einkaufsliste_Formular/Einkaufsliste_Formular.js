7f9fcab3f5e7105fd402b6aa28eeedac60e35779
Caroco44 <caroline04kraemer@yahoo.de>
2024-05-02 10:55:02 +0200

    240502_1

--- A03_Einkaufsliste_Formular/Einkaufsliste_Formular.js
+++ A03_Einkaufsliste_Formular/Einkaufsliste_Formular.js
@@ -18,30 +18,57 @@ var Einkaufsliste;
     newEntryButton.addEventListener("pointerdown", function () {
         console.log("New Entry wurde geklickt");
     });
-    Einkaufsliste.data = [
-        { product: "Milk", checked: true, purchasedButton: "clicked", comment: "cow milk", date: "25.04.2024", amount: 2 },
-        { product: "Bread", checked: true, purchasedButton: "clicked", comment: "cow milk", date: "25.04.2024", amount: 2 }
+    let data = [
+        { item: "Milk", checked: true, purchased: "clicked", comment: "cow milk", date: "25.04.2024", amount: 2, deleteBtn: "clicked" },
+        { item: "Bread", checked: true, purchased: "clicked", comment: "white", date: "25.04.2024", amount: 2, deleteBtn: "clicked" },
     ];
-    for (let entry of Einkaufsliste.data) {
-        createNew(entry);
+    for (let entry of data) {
+        createNewEntry(entry);
     }
-    function createNew(_entry) {
+    function createNewEntry(_entry) {
         console.log(_entry);
-        // let group: HTMLDivElement = document.createElement("div");
-        // for (let item of _items) {
-        //   let checkbox: HTMLInputElement = document.createElement("input");
-        //   checkbox.type = "checkbox";
-        //   checkbox.setAttribute("price", item.price.toFixed(2));
-        //   checkbox.value = item.name;
-        //   checkbox.name = _category;
-        //   checkbox.id = item.name;
-        //   let label: HTMLLabelElement = document.createElement("label");
-        //   label.textContent = item.name;
-        //   label.htmlFor = item.name;
-        //   group.appendChild(checkbox);
-        //   group.appendChild(label);
-        // }
-        // return group;
+        let group = document.createElement("div");
+        let item = document.createElement("input");
+        item.type = "text";
+        item.placeholder = "Product";
+        item.value = _entry.item;
+        group.appendChild(item);
+        let checked = document.createElement("input");
+        checked.type = "checkbox";
+        checked.name = _entry.item;
+        checked.className = "checkboxClass";
+        checked.checked = _entry.checked;
+        group.appendChild(checked);
+        let purchased = document.createElement("button");
+        purchased.className = "purchasedButton";
+        purchased.type = "button";
+        purchased.textContent = "Purchased";
+        group.appendChild(purchased);
+        let comment = document.createElement("input");
+        comment.type = "text";
+        comment.placeholder = "Comment";
+        comment.value = _entry.comment;
+        group.appendChild(comment);
+        let date = document.createElement("input");
+        date.type = "text";
+        date.placeholder = "Date";
+        date.value = _entry.date;
+        group.appendChild(date);
+        let amount = document.createElement("input");
+        amount.type = "number";
+        amount.name = "Stepper";
+        amount.step = "1";
+        amount.min = "0";
+        amount.max = "100";
+        amount.value = _entry.amount.toString();
+        group.appendChild(amount);
+        let deleteBtn = document.createElement("button");
+        deleteBtn.className = "deleteButton";
+        deleteBtn.type = "button";
+        deleteBtn.textContent = "Delete";
+        group.appendChild(deleteBtn);
+        document.body.appendChild(group);
+        return group;
     }
 })(Einkaufsliste || (Einkaufsliste = {}));
 //# sourceMappingURL=Einkaufsliste_Formular.js.map
---
+++ A08_GenerativeArt/GenerativeArt.js
@@ -0,0 +1,160 @@
+"use strict";
+var A08_GenerativeArt;
+(function (A08_GenerativeArt) {
+    window.addEventListener("load", handleLoad);
+    let crc2;
+    function handleLoad(_event) {
+        let canvas = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2 = canvas.getContext("2d");
+        // let horizon: number = crc2.canvas.height * golden
+        drawBackground();
+        drawRhombus({ x: canvas.width, y: canvas.height });
+        drawCircles({ x: canvas.width, y: canvas.height });
+        drawLights({ x: canvas.width, y: canvas.height });
+        // drawSun({ x: 100, y: 75 });
+        // drawStreet({ x: crc2.canvas.width / 2, y: horizon }, 100, 600);
+        // drawMountains({ x: 0, y: horizon }, 75, 200, "grey", "white");
+        // drawMountains({ x: 0, y: horizon }, 50, 150, "grey", "lightgrey");
+    }
+    function drawBackground() {
+        console.log("Background");
+        let gradient = crc2.createRadialGradient(crc2.canvas.width / 2, crc2.canvas.height / 2, // Mittelpunkt
+        0, // Radius des Startkreises
+        crc2.canvas.width / 2, crc2.canvas.height / 2, // Mittelpunkt
+        Math.max(crc2.canvas.width, crc2.canvas.height) // Radius des Endkreises (maximaler Abstand zum Mittelpunkt)
+        );
+        gradient.addColorStop(0.4, "black");
+        gradient.addColorStop(0.9, "blue");
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
+    function drawRhombus(_position) {
+        console.log("Rhombus", _position);
+        let nParticles = 6;
+        let gradient = crc2.createLinearGradient(100, 100, 120, 150);
+        gradient.addColorStop(0, "rgb(11, 203, 139)");
+        gradient.addColorStop(1, "rgb(41, 148, 231)");
+        crc2.fillStyle = gradient;
+        crc2.save();
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = Math.random() * _position.x;
+            let y = Math.random() * _position.y;
+            crc2.translate(x, y);
+            crc2.beginPath();
+            crc2.moveTo(100, 100);
+            crc2.lineTo(120, 150);
+            crc2.lineTo(100, 190);
+            crc2.lineTo(80, 150);
+            crc2.closePath();
+            crc2.fill();
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+    function drawCircles(_position) {
+        console.log("Circles", _position);
+        let nParticles = 10;
+        let radiusParticle = 40;
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "black");
+        gradient.addColorStop(1, "blue");
+        crc2.save();
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = Math.random() * _position.x;
+            let y = Math.random() * _position.y;
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+    function drawLights(_position) {
+        console.log("Lights", _position);
+        let nParticles = 7;
+        let radiusParticle = 13;
+        let particle = new Path2D();
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        crc2.save();
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = Math.random() * _position.x;
+            let y = Math.random() * _position.y;
+            crc2.translate(x, y);
+            // Generate a random number to determine the color
+            let color = Math.random();
+            // Set up the gradient based on the color
+            let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+            gradient.addColorStop(0, "white");
+            if (color > 0.5) {
+                gradient.addColorStop(1, "yellow");
+            }
+            else {
+                gradient.addColorStop(1, "rgb(185, 7, 173)"); // pink
+            }
+            crc2.fillStyle = gradient;
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+    // function drawSun(_position: Vector): void {
+    //   console.log("Sun", _position);
+    //   let r1: number = 30;
+    //   let r2: number = 150;
+    //   let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+    //   gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
+    //   gradient.addColorStop(1, "HSLA(60, 100%, 50%, 0)");
+    //   crc2.save();
+    //   crc2.translate(_position.x, _position.y);
+    //   crc2.fillStyle = gradient;
+    //   crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+    //   crc2.fill();
+    //   crc2.restore();
+    // }
+    // function drawStreet(_position: Vector, _widthBack: number, _widthFront: number): void {
+    //   console.log("Street");
+    //   crc2.beginPath();
+    //   crc2.moveTo(_position.x + _widthBack / 2, _position.y);
+    //   crc2.lineTo(crc2.canvas.width / 2 + _widthFront / 2, crc2.canvas.height);
+    //   crc2.lineTo(crc2.canvas.width / 2 - _widthFront / 2, crc2.canvas.height);
+    //   crc2.lineTo(_position.x - _widthBack / 2, _position.y);
+    //   crc2.closePath();
+    //   let gradient: CanvasGradient = crc2.createLinearGradient(0, _position.y, 0, crc2.canvas.height);
+    //   gradient.addColorStop(0, "darkgrey");
+    //   gradient.addColorStop(0.6, "black");
+    //   crc2.fillStyle = gradient;
+    //   crc2.fill();
+    // }
+    // function drawMountains(_position: Vector, _min: number, _max: number, _colorLow: string, _colorHigh: string): void {
+    //   console.log("Mountains");
+    //   let stepMin: number = 50;
+    //   let stepMax: number = 150;
+    //   let x: number = 0;
+    //   crc2.save();
+    //   crc2.translate(_position.x, _position.y);
+    //   crc2.beginPath();
+    //   crc2.moveTo(0, 0);
+    //   crc2.lineTo(0, - _max);
+    //   do {
+    //     x += stepMin + Math.random() * (stepMax - stepMin);
+    //     let y: number = - _min - Math.random() * (_max - _min);
+    //     crc2.lineTo(x, y);
+    //   } while (x < crc2.canvas.width);
+    //   crc2.lineTo(x, 0);
+    //   crc2.closePath();
+    //   let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, - _max);
+    //   gradient.addColorStop(0, _colorLow);
+    //   gradient.addColorStop(0.7, _colorHigh);
+    //   crc2.fillStyle = gradient;
+    //   crc2.fill();
+    //   crc2.restore();
+    // }
+})(A08_GenerativeArt || (A08_GenerativeArt = {}));
+//# sourceMappingURL=GenerativeArt.js.map
