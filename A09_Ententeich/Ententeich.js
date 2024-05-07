e60726efac915a9f56a3edaa1a809498aa9629e6
Caroco44 <caroline04kraemer@yahoo.de>
2024-05-07 17:54:44 +0200

    240507_2

--- A09_Ententeich/Ententeich.js
+++ A09_Ententeich/Ententeich.js
@@ -3,7 +3,7 @@ var L08_Canvas_Alley;
 (function (L08_Canvas_Alley) {
     window.addEventListener("load", handleLoad);
     let crc2;
-    let golden = 0.62;
+    let line = 0.46;
     function handleLoad(_event) {
         let canvas = document.querySelector("canvas");
         if (!canvas)
@@ -11,30 +11,31 @@ var L08_Canvas_Alley;
         crc2 = canvas.getContext("2d");
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        let horizon = crc2.canvas.height * golden;
+        let horizon = crc2.canvas.height * line;
         drawBackground();
         drawSun({ x: 100, y: 75 });
         drawCloud({ x: 500, y: 125 }, { x: 250, y: 75 });
-        drawStreet({ x: crc2.canvas.width / 2, y: horizon }, 100, 600);
-        drawMountains({ x: 0, y: horizon }, 75, 200, "grey", "white");
-        drawMountains({ x: 0, y: horizon }, 50, 150, "grey", "lightgrey");
+        // drawStreet({ x: crc2.canvas.width / 2, y: horizon }, 100, 600);
+        drawMountains({ x: 0, y: horizon }, 75, 200, "rgb(215, 27, 222)", "rgb(255, 198, 246)");
+        drawMountains({ x: 0, y: horizon }, 50, 150, "rgb(218, 15, 225)", "rgb(255, 239, 255)");
     }
     function drawBackground() {
         console.log("Background");
         let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
-        gradient.addColorStop(0, "lightblue");
-        gradient.addColorStop(golden, "white");
-        gradient.addColorStop(1, "HSL(100, 80%, 30%)");
+        gradient.addColorStop(0, "purple");
+        gradient.addColorStop(0.3, "rgb(215, 27, 222)");
+        gradient.addColorStop(0.7, "rgb(16, 138, 110)");
+        gradient.addColorStop(1, "rgb(11, 203, 139)");
         crc2.fillStyle = gradient;
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
     }
     function drawSun(_position) {
         console.log("Sun", _position);
-        let r1 = 30;
+        let r1 = 50;
         let r2 = 150;
         let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
-        gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
-        gradient.addColorStop(1, "HSLA(60, 100%, 50%, 0)");
+        gradient.addColorStop(0, "rgba(238, 182, 188, 0.6)");
+        gradient.addColorStop(1, "rgba(250, 118, 131, 0)");
         crc2.save();
         crc2.translate(_position.x, _position.y);
         crc2.fillStyle = gradient;
@@ -49,8 +50,8 @@ var L08_Canvas_Alley;
         let particle = new Path2D();
         let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
         particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
-        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
-        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+        gradient.addColorStop(0, "rgba(240, 227, 240, 0.5)");
+        gradient.addColorStop(1, "rgba(240, 227, 240, 0)");
         crc2.save();
         crc2.translate(_position.x, _position.y);
         crc2.fillStyle = gradient;
@@ -64,20 +65,20 @@ var L08_Canvas_Alley;
         }
         crc2.restore();
     }
-    function drawStreet(_position, _widthBack, _widthFront) {
-        console.log("Street");
-        crc2.beginPath();
-        crc2.moveTo(_position.x + _widthBack / 2, _position.y);
-        crc2.lineTo(crc2.canvas.width / 2 + _widthFront / 2, crc2.canvas.height);
-        crc2.lineTo(crc2.canvas.width / 2 - _widthFront / 2, crc2.canvas.height);
-        crc2.lineTo(_position.x - _widthBack / 2, _position.y);
-        crc2.closePath();
-        let gradient = crc2.createLinearGradient(0, _position.y, 0, crc2.canvas.height);
-        gradient.addColorStop(0, "darkgrey");
-        gradient.addColorStop(0.6, "black");
-        crc2.fillStyle = gradient;
-        crc2.fill();
-    }
+    // function drawStreet(_position: Vector, _widthBack: number, _widthFront: number): void {
+    //   console.log("Street");
+    //   crc2.beginPath();
+    //   crc2.moveTo(_position.x + _widthBack / 2, _position.y);
+    //   crc2.lineTo(crc2.canvas.width / 2 + _widthFront / 2, crc2.canvas.height);
+    //   crc2.lineTo(crc2.canvas.width / 2 - _widthFront / 2, crc2.canvas.height);
+    //   crc2.lineTo(_position.x - _widthBack / 2, _position.y);
+    //   crc2.closePath();
+    //   let gradient: CanvasGradient = crc2.createLinearGradient(0, _position.y, 0, crc2.canvas.height);
+    //   gradient.addColorStop(0, "darkgrey");
+    //   gradient.addColorStop(0.6, "black");
+    //   crc2.fillStyle = gradient;
+    //   crc2.fill();
+    // }
     function drawMountains(_position, _min, _max, _colorLow, _colorHigh) {
         console.log("Mountains");
         let stepMin = 50;
